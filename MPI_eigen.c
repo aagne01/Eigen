1ddf3c7ed7eb5ee8062a42a4ee6b6275ca08d319
Amadou Agne <amag7207@rackham3.uppmax.uu.se>
2017-05-15 23:54:10 +0200

    Writing the body of the code

--- MPI_eigen.c
+++ MPI_eigen.c
@@ -17,9 +17,13 @@ int main(int argc, char *argv[]){
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
   if(rank == 0) {
-
+  /*We initialize the matrix A and send rows of A to the other processes */
  
   }
+
+  /* After receiving rows of A, we start the power method and use Allgather to send y to all processes */
+
+  /*When the algorithm terminates we print the dominant eigein value and the associated eigen vector in root processor */ 
   
   MPI_Finalize();
 
