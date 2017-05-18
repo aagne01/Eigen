119b4d8b67d9e8fd8462b96bb45674d97fde8b41
Amadou Agne <amag7207@rackham4.uppmax.uu.se>
2017-05-18 19:12:31 +0200

    starting to implement the power method

--- MPI_eigen.c
+++ MPI_eigen.c
@@ -36,7 +36,7 @@ double compute_norm(int vect_length,double x[vect_length]){
     
     norm = sqrt(norm_sqr); 
     return norm;
-    }
+}
 
 void norm_vect(int vect_length,double x[vect_length]){
     
@@ -46,8 +46,7 @@ void norm_vect(int vect_length,double x[vect_length]){
    
     for (i = 0; i < vect_length; i++){
         x[i] = x[i] / norm ;
-    }
-   
+    }  
 }
 
 
@@ -55,25 +54,21 @@ void norm_vect(int vect_length,double x[vect_length]){
 
 void mat_mult(int heigth, int length, double A_rows[length*heigth], double x[length], double y[heigth]){
 
-    int  i, k;
+    int  i, j;
     
-    for(i = 0; i<heigth; i++){
-        for(k = 0; k<length ; k++){
-           y[i] += A_rows[i*length + k] * x[k] ;
+    for(i = 0; i < heigth; i++){
+        for(j = 0; j < length ; j++){
+           y[i] += A_rows[i*length + j] * x[j] ;
         }
     }    
 }
 
 
-
-
-
-
-
 int main(int argc, char *argv[]){
 
-  int rank, nprocs, mat_size, blockrows_size, i, j ;
-
+  int rank, nprocs, mat_size, blockrows_size, i, j, k, M ;
+  double lambda, lambda0, e; 
+ 
   mat_size = atoi(argv[1]);
    
   //double x[2] = {1.0,1.0}; test of the norm_vect function
@@ -83,14 +78,19 @@ int main(int argc, char *argv[]){
   double *A;
   double *x;
   double *y;
+  double *y_blocks;
 
-
+  
 
   x = malloc(mat_size*sizeof(double));
- 
-
-  memset(x,0,mat_size*sizeof(double));
-  x[4] = 1.0;
+  
+  M = 2;
+  e = 1e-6; //e is the tolerance
+  lambda = 0;
+  lambda0 = lambda + 2*e;
+  k = 0;
+  
+  
 
   MPI_Request req_A_send;
   MPI_Request req_A_recv;
@@ -102,7 +102,8 @@ int main(int argc, char *argv[]){
   blockrows_size = mat_size / nprocs ;
 
 
-  y= malloc(blockrows_size*sizeof(double));
+  y = malloc(mat_size*sizeof(double));
+  y_blocks = malloc(blockrows_size*sizeof(double));
 
   /* Define new type to send datas */
   MPI_Datatype blocktype, blockselect;
@@ -116,6 +117,13 @@ int main(int argc, char *argv[]){
   if(rank == 0) {
     /*We initialize the matrix A and send rows of A to the other processes */
     A = (double *)malloc(sizeof(double) * mat_size * mat_size);
+    for(i = 0; i < mat_size; i++){
+        x[i]=1.0;
+    }
+    
+    Prvalues(mat_size,1,x);
+    norm_vect(mat_size,x);
+    Prvalues(mat_size,1,x);
 
     
     /* Matrix generation*/
@@ -137,27 +145,35 @@ int main(int argc, char *argv[]){
      /* double norm = compute_norm(2,x);
       printf("norm %.3f\t \n",norm);         // Testing the normalization of the vectors
       norm_vect(2,x);
-      Prvalues(1,2,x);*/
-     
-            
-
+      Prvalues(1,2,x);*/       
   }
 
+  
 
   /* After receiving rows of A, we start the power method and use Allgather to send y to all processes */
   A_blockrows = (double *)malloc(mat_size * blockrows_size * sizeof(double));
   MPI_Irecv(A_blockrows, 1, blocktype, 0, 3, MPI_COMM_WORLD, &req_A_recv);
 
   MPI_Wait(&req_A_recv, MPI_STATUS_IGNORE);
-  sleep(1);
-  if(rank == 2)mat_mult( blockrows_size,mat_size,A_blockrows,x,y);
   
 
+  MPI_Bcast(&x[0],mat_size,MPI_DOUBLE,0,MPI_COMM_WORLD);
+  //if(rank == 2)mat_mult( blockrows_size,mat_size,A_blockrows,x,y);
+
+  
+  while (abs(lambda-lambda0) >= e && k <= M ){
+
+    k += 1;
+  }
+  
+  
+  
 
   /* Printing Part*/
   sleep(rank);
   printf("rank %d \n", rank);
-  if (rank == 2) Prvalues(1, blockrows_size, y);
+  Prvalues(mat_size,1,x);
+  //if (rank == 2) Prvalues(1, blockrows_size, y);
   Prvalues(mat_size, blockrows_size, A_blockrows);
 
   /*When the algorithm terminates we print the dominant eigein value and the associated eigen vector in root processor */ 
--- Makefile
+++ Makefile
@@ -7,5 +7,10 @@ LIBS    = -lmpi -lm
 MPI_eigen: MPI_eigen.c
 	$(CC) $(CCFLAGS) -o MPI_eigen MPI_eigen.c $(LIBS)
 
+test_bcast: test_bcast.c
+	$(CC) $(CCFLAGS) -o test_bcast test_bcast.c $(LIBS)
+
 clean:
 	$(RM) MPI_eigen
+	$(RM) test_bcast
+	$(RM) core.*
