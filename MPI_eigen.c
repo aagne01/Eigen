b1b2649924ae61c5ea9f5f36eb499ba86c33da98
Amadou Agne <amag7207@rackham2.uppmax.uu.se>
2017-05-22 21:12:42 +0200

    code works but poor performance, did some cleaning as well

--- MPI_eigen.c
+++ MPI_eigen.c
@@ -1,154 +1,189 @@
-#include <mpi.h>
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
 #include <math.h>
-#include <unistd.h>
-#include <time.h>
-#include <string.h>
+
 
 
 void Prvalues(int length, int heigth,  double matrix[length * heigth]);
-double compute_norm(int vect_length,double x[vect_length]);
-void norm_vect(int vect_length,double x[vect_length]);
-void mat_mult(int length, double A_rows[], double x[], double y[]);
-double power_method(double A[],int n_iterations,int mat_size);
-void generate_matrix(double matrix[],int mat_size);
+void mat_mult(double A_rows[], double vect[], double result[], int length);
+double compute_norm2(double vect[], int vect_length);
+double powerMethod(double A_rows[], int n_times, int mat_size);
 
 
-int main(int argc, char *argv[]){
 
-  int rank, nprocs, mat_size, blockrows_size, i, j, M ;
-  //double lambda, e;
+void generateMatrix(double A_rows[], int mat_size);
+
+
+int main(int argc, char* argv[]) {
+
+  /* Set parameters */
+  int mat_size = atoi(argv[1]);
+  int n_times = atoi(argv[2]);
+
+
+  /* Start parallel calculations */
+  int rank, nprocs;
+  MPI_Init(&argc, & argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+
+  /* p has to be a divisor of N !! */
+
+
  
-  mat_size = atoi(argv[1]);
-   
+  int blockrows_size = mat_size / nprocs;
 
-/*  double *A_blockrows;
-  double *A;
- */ 
-  
-  M = atoi(argv[2]);;
-//  e = 1e-6; //e is the tolerance
- // lambda = 0;
-  
-  
-  
-
- /* MPI_Request req_A_send;
-  MPI_Request req_A_recv;
-*/
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-
-  blockrows_size = mat_size / nprocs ;
-
+  /* Generate the matrix */
   double A_blocks[mat_size*blockrows_size];
-  generate_matrix(A_blocks,mat_size);
-  /* Define new type to send datas */
- /* MPI_Datatype blocktype, blockselect;
-
-  MPI_Type_contiguous(mat_size*blockrows_size, MPI_DOUBLE, &blocktype);
-  MPI_Type_commit(&blocktype);
-
-  MPI_Type_vector(blockrows_size, mat_size, mat_size, MPI_DOUBLE, &blockselect);
-  MPI_Type_commit(&blockselect);
-*/
- // if(rank == 0) {
-    /*We initialize the matrix A and send rows of A to the other processes */
-  //  A = (double *)malloc(sizeof(double) * mat_size * mat_size);
-
-    
-    /* Matrix generation*/
-   /* for(i = 0; i < mat_size; i++){
-       for(j = 0; j < mat_size; j++){
-         // if (i == j){
-             A[i*mat_size + j] = 1;
-         // }*/
-        /*  else if(i+1 == j && i < mat_size-1){
-             A[i*mat_size + j] = mat_size;
-          }
-          else if(i == j+1 && j < mat_size-1){
-             A[i*mat_size + j] = mat_size;
-          }
-          else{
-             A[i*mat_size +j] = 0;
-          }*/
-     //  }    
-   // }
-   // Prvalues(mat_size, mat_size, A);
-    
- 
-    /* Distribution of Block_rows of A */
-    /* for (i = 0; i < nprocs; i++){
-            int i_b_m = i * blockrows_size * mat_size;
-            MPI_Isend(&A[i_b_m], 1, blockselect, i, 3, MPI_COMM_WORLD, &req_A_send);
-            
-     }*/
-     
-     /* double norm = compute_norm(2,x);
-      printf("norm %.3f\t \n",norm);         // Testing the normalization of the vectors
-      norm_vect(2,x);
-      Prvalues(1,2,x);*/       
- // }
-
-  
-
-  /* After receiving rows of A, we start the power method and use Allgather to send y to all processes */
- /* A_blockrows = (double *)malloc(mat_size * blockrows_size * sizeof(double));
-  MPI_Irecv(A_blockrows, 1, blocktype, 0, 3, MPI_COMM_WORLD, &req_A_recv);
-
-  MPI_Wait(&req_A_recv, MPI_STATUS_IGNORE);
-  */
-   
-   /*for(j = 0; j<mat_size; j++){
-      for(i = 0; i<blockrows_size; i++){
-     
-         if (j-rank*blockrows_size>i){
-             A_blocks[j + i*mat_size] = 0; 
-         } 
-         else{
-             A_blocks[j + i*mat_size] = rank*(blockrows_size) + i + 1;
-         }
-       }
-   } */ 
- 
-    
-  double lambda = power_method(A_blocks,M,mat_size);
-  
-  
-
-  /* Printing Part*/
-  sleep(rank);
-  printf("rank %d \n", rank);
-  //Prvalues(mat_size,1,x);
-  //Prvalues(blockrows_size,1,y_blocks);
-  //Prvalues(mat_size,1,y);
-  if (rank == 0) printf("lambda = %.2f\t \n",lambda);
-  //if (rank == 2) Prvalues(1, blockrows_size, y);
-  Prvalues(mat_size, blockrows_size, A_blocks);
-
-  /*When the algorithm terminates we print the dominant eigein value and the associated eigen vector in root processor */ 
-
-  /*free(y_blocks);
-  free(y);
-  free(x);
-  free(A_blockrows);*/
+  generateMatrix(A_blocks,mat_size);
 
 
- /* MPI_Type_free(&blockselect);
-  MPI_Type_free(&blocktype);
-  */
+  /* Run the powerMethod algorithm */
+  double start = MPI_Wtime();
+  double lambda = powerMethod(A_blocks,n_times,mat_size);
+  double stop = MPI_Wtime();
+
+
+
+  /* Calculating times */
+  double timediff = stop - start;
+  double times[nprocs];
+  double average = 0;
+  MPI_Gather(&timediff,1,MPI_DOUBLE,times,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
+  if(rank==0){
+    for(int i=0; i<nprocs; i++)
+      average += times[i];
+    average /= nprocs;
+  }
+
+
+  /* Print the results */
+  if(rank==0)
+    printf("dimension\t%d\tdominant lambda\t%f\ttime\t%f\n",mat_size,lambda,average);
+
+
+  /* End MPI */
   MPI_Finalize();
 
-
   return 0;
+}
+
+
+
+void generateMatrix(double A_rows[], int mat_size){
+
+  int rank, nprocs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+  /* Define the matrix on the (different) processors */
+  for(int j=0; j<mat_size  ; j++) {
+  for(int i=0; i<mat_size/nprocs; i++) {
+
+    if(j-rank*mat_size/nprocs>i)
+      A_rows[j+mat_size*i] = 0;
+    else
+      A_rows[j+mat_size*i] = rank*(mat_size/nprocs)+i+1;
+
+  }}
+
+}
+
+
+double compute_norm2(double vect[], int vect_length){
+
+  /* Add the square of each element */
+  double result = 0;
+  for(int i=0; i<vect_length;i++)
+    result += pow(vect[i],2);
+
+  /* The norm of the vector is the square root of the sum */
+  return pow(result,0.5);
+}
+
+
+
+void mat_mult(double A_rows[], double vect[], double result[], int mat_size){
+
+  int rank, nprocs;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+
+  /* Broadcast the vector defined on pr0 */
+  MPI_Bcast(vect, mat_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+  /* Set temporary result to zero */
+  double y_i[mat_size/nprocs];
+  for(int i=0; i<mat_size/nprocs; i++)
+    y_i[i]=0;
+
+
+  /* Perform the actual matrix multiplication in parallel */
+  for(int i=0; i<mat_size/nprocs; i++)
+  for(int j=0; j<mat_size  ; j++)
+    y_i[i] += A_rows[j+mat_size*i] * vect[j];
+
+
+  /* Gather the results from the different processors */
+  MPI_Gather(y_i,mat_size/nprocs,MPI_DOUBLE,result,mat_size/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);
 
 }
 
 
 
-/* Function to Print matrix */
+double powerMethod(double A_rows[], int n_times, int mat_size) {
+
+  int rank;
+  double lambda=0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  
+
+  /* Define starting vector */
+  double x[mat_size];
+  if(rank==0){
+    
+    for(int i=0; i<mat_size; i++)
+      x[i]=1;
+  }
+
+
+  /* The actual iteration process */
+  for(int n=0; n<n_times; n++){
+
+
+    /* Normalize the vector*/
+    if(rank==0){
+      double norm = compute_norm2(x,mat_size);
+      for(int i=0; i<mat_size; i++)
+	x[i] = x[i]/norm;
+    }
+
+
+    /* Calculate the product of the matrix and the vector */
+    double x_tmp[mat_size];
+    mat_mult(A_rows,x,x_tmp,mat_size);
+    if(rank==0){
+      for(int i=0; i<mat_size; i++)
+	x[i] = x_tmp[i];
+      }
+      lambda = compute_norm2(x,mat_size);
+    }
+
+  /* The result is the norm of the vector */
+  
+  return lambda;
+}
+
+
+
+
+/* Function to Print matrix and vectors*/
 void Prvalues(int length, int heigth,  double matrix[length * heigth]){   
     int i, j;
     printf("\n");
@@ -159,133 +194,4 @@ void Prvalues(int length, int heigth,  double matrix[length * heigth]){
         printf("\n");
     }
     printf("\n");
-}
-
-
-/* Functions to normalize a vector */
-
-double compute_norm(int vect_length,double x[vect_length]){
-       
-    int i;
-    double norm, norm_sqr;
-    norm = 0;
-    norm_sqr = 0;    
-
-    for (i = 0; i < vect_length; i++){
-        norm_sqr += x[i]*x[i] ;
-    }
-    
-    norm = sqrt(norm_sqr); 
-    return norm;
-}
-
-void norm_vect(int vect_length,double x[vect_length]){
-    
-    int i;
-    double  norm ;
-    norm = compute_norm(vect_length,x) ;
-   
-    for (i = 0; i < vect_length; i++){
-        x[i] = x[i] / norm ;
-    }  
-}
-
-
-/* Function to multiply the sub row-matrix by the vector x and store it in x*/
-
-void mat_mult( int length, double A_rows[], double x[], double y[]){
-
-    int  i, j, rank, nprocs;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-
-
-    /* Broadcast the column vector from proc 0 to all processes */
-    MPI_Bcast(x,length,MPI_DOUBLE,0,MPI_COMM_WORLD);
-
-    double tmp[length/nprocs];      
-    
-    for(i = 0; i < length/nprocs; i++){
-        for(j = 0; j < length ; j++){
-           tmp[i] += A_rows[i*length + j] * x[j] ;
-        }
-    }
-
-    MPI_Gather(tmp,length/nprocs,MPI_DOUBLE,y,length/nprocs,MPI_DOUBLE,0,MPI_COMM_WORLD);    
-}
-
-double power_method(double A_blocks[],int n_iterations,int mat_size){
-
-    int rank, i, n;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    double lambda = 0;
-   // double lambda0 = lambda + 2*e ;
-   // int k = 0 ;
-
-    /* Define starting vector */
-    double x[mat_size];
-    if(rank==0){
-      for(i = 0; i < mat_size; i++){
-         x[i] = 1;
-      }
-    }
-
-    
-     
-   // while (abs(lambda-lambda0) >= e && k <= n_iterations ){
-    for(n = 0; n < n_iterations; n++){
-
-        
-        //Prvalues(mat_size,1,x);
-
-        if (rank == 0){
-        norm_vect(mat_size,x);
-        }
-
-       
-        //Prvalues(mat_size,1,x);
-        double  tmp[mat_size];
-        mat_mult(mat_size, A_blocks, x, tmp);
-
-                
-
-        if(rank ==0){
-        //lambda0 = lambda ;
-          for(i = 0; i < mat_size; i++){
-              x[i] = tmp[i] ;             
-             }
-        
-        }
-        
-        
-    }
-
-  
-        lambda = compute_norm(mat_size, x) ;  
-        return lambda ;
-}
-
-
-
-void generate_matrix(double matrix[], int mat_size){
-  
-     int i,j, rank, nprocs;
-
-     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-     MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-
-     for(j=0; j< mat_size; j++){
-        for(i=0; i<mat_size/nprocs;i++){
-            if(j-rank*(mat_size/nprocs)>i){
-                 matrix[i*mat_size + j] = 0;
-            }
-            else{
-                 matrix[i*mat_size + j] = rank*(mat_size/nprocs)+ i +1;
-            }
-        }
-     }
-
-
-
 }
--- Makefile
+++ Makefile
@@ -4,13 +4,17 @@ CCFLAGS = -O3 -Wall -g
 
 LIBS    = -lmpi -lm
 
+
 MPI_eigen: MPI_eigen.c
 	$(CC) $(CCFLAGS) -o MPI_eigen MPI_eigen.c $(LIBS)
 
 test_bcast: test_bcast.c
 	$(CC) $(CCFLAGS) -o test_bcast test_bcast.c $(LIBS)
 
+
+
 clean:
 	$(RM) MPI_eigen
 	$(RM) test_bcast
 	$(RM) core.*
+
